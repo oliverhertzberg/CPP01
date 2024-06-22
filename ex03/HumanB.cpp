c2ebb84d3ce5984fed2f0fea41a6662ecd0aec13
Oliver Hertzberg <ohertzbe@c2r1p10.hive.fi>
2024-06-22 19:51:20 +0300

    now it works

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,7 @@
 /*   By: ohertzbe <ohertzbe@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 19:02:04 by ohertzbe          #+#    #+#             */
-/*   Updated: 2024/06/22 19:23:27 by ohertzbe         ###   ########.fr       */
+/*   Updated: 2024/06/22 19:49:54 by ohertzbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ HumanB::~HumanB(){}
 void    HumanB::attack(){
     std::cout << this->name << " attacks with their ";
     if (this->type != nullptr)
-        std::cout << this->type << std::endl;
+        std::cout << this->type->getType() << std::endl;
     else
         std::cout << std::endl;
 }
 
 void    HumanB::setWeapon(Weapon& weapon){
-    *(this->type) = weapon;
+    this->type = &weapon;
 }
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: ohertzbe <ohertzbe@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 19:27:19 by ohertzbe          #+#    #+#             */
-/*   Updated: 2024/06/22 19:28:10 by ohertzbe         ###   ########.fr       */
+/*   Updated: 2024/06/22 19:36:00 by ohertzbe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ class Weapon{
     
     public:
         Weapon();
-        Weapon::Weapon(std::string weapon);
+        Weapon(std::string weapon);
         ~Weapon();
         
         const std::string& getType();
